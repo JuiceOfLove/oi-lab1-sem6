e91ccc8b6344f79505fab0d8ba1ad2127caef2f7
Андрей Цыбуленко <andrey_csgo26@mail.ru>
2024-02-08 16:45:38 +0300

    init

---
+++ src/components/screen/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './mainPage.module.css'
+
+function MainPage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}
+
+export default MainPage
